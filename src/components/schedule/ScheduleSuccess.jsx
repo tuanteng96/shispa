9ed839985c6bd12183cdba6b29bce49631fa9757
tuanteng96 <tuantengyn@gmail.com>
2021-07-14 17:46:36 +0700

    update

--- src/components/schedule/ScheduleSuccess.jsx
+++ src/components/schedule/ScheduleSuccess.jsx
@@ -11,10 +11,10 @@ export default class ScheduleSuccess extends React.Component {
     return (
       <div className="page-schedule__success">
         <GrCheckmark />
-        <h4>Đặt lịch thành công</h4>
         <div className="desc">
-          <span>Chúc mừng bạn đã đặt lịch thành công.</span>
-          <span>Vui lòng chờ đợi. Chúng tôi sẽ xác nhận đặt lịch của bạn.</span>
+          <div>Quý khách vui lòng chờ trong ít phút.</div>
+          <div>Chúng tôi sẽ liên hệ lại <span>xác nhận đặt lịch</span> của Quý Khách qua tin nhắn App hoặc điện thoại.</div>
+          <div>Xin cảm ơn quý khách !</div>
         </div>
         <Link
           noLinkClass
